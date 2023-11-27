2508057d78be6cd949e373d7432649446c1fe547
NeginKhalifat <negin99kh@gmail.com>
2023-11-27 10:47:59 -0700

    refactor limit

--- query_generation/limit/limit_generator.py
+++ query_generation/limit/limit_generator.py
@@ -2,6 +2,21 @@ import random
 
 
 def generate_limit_clause(limit_type):
+    """
+    Generate the LIMIT clause based on the given limit_type.
+
+    Args:
+        limit_type (str): The type of LIMIT clause to generate.
+
+    Returns:
+        str: The generated LIMIT clause.
+
+    Examples:
+        >>> generate_limit_clause("without_offset")
+        '5'
+        >>> generate_limit_clause("with_offset")
+        '7 OFFSET 3'
+    """
     limit_type_mapping = {
         "without_offset": generate_limit_clause_without_offset,
         "with_offset": generate_limit_clause_with_offset,
@@ -10,8 +25,28 @@ def generate_limit_clause(limit_type):
 
 
 def generate_limit_clause_without_offset():
+    """
+    Generate the LIMIT clause without OFFSET.
+
+    Returns:
+        str: The generated LIMIT clause.
+
+    Examples:
+        >>> generate_limit_clause_without_offset()
+        '5'
+    """
     return str(random.randint(1, 10))
 
 
 def generate_limit_clause_with_offset():
+    """
+    Generate the LIMIT clause with OFFSET.
+
+    Returns:
+        str: The generated LIMIT clause.
+
+    Examples:
+        >>> generate_limit_clause_with_offset()
+        '7 OFFSET 3'
+    """
     return f"{random.randint(1, 10)} OFFSET {random.randint(1, 10)}"

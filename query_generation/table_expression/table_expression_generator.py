b340f8c3b998d87bec561b2709c0e7348de1a293
NeginKhalifat <negin99kh@gmail.com>
2023-11-27 15:17:28 -0700

    refactoring table expression

--- query_generation/table_expression/table_expression_generator.py
+++ query_generation/table_expression/table_expression_generator.py
@@ -6,8 +6,17 @@ import networkx as nx
 
 sys.path.append("..")
 
+import random
+
 from helper_funcs import all_colms
 from join import generate_join_query, generate_meaningless_join
+
+from .table_expression_helper_funcs import (
+    handle_join_case,
+    handle_single_table_case,
+    handle_single_table_with_name_changing_case,
+    handle_table_expression_for_subquery,
+)
 
 
 def create_table_expression(
@@ -23,135 +32,43 @@ def create_table_expression(
     Generate SQL table expression based on the specified type.
 
     Args:
-    - schema (dict): Database schema with table names and their columns.
-    - pk (dict): Primary keys for tables.
-    - fk (dict): Foreign keys for tables.
-    - schema_types (dict): Data types of columns in the schema.
-    - table_expression_type (str or dict): Type of table expression to generate.
-    - meaningful_joins (str): Type of join - meaningful or meaningless.
+        schema (dict): Database schema with table names and their columns.
+        pk (dict): Primary keys for tables.
+        fk (dict): Foreign keys for tables.
+        schema_types (dict): Data types of columns in the schema.
+        table_expression_type (str or dict): Type of table expression to generate.
+        meaningful_joins (str): Type of join - meaningful or meaningless.
+        random_choice (bool, optional): Indicates if random choice is enabled. Defaults to False.
 
     Returns:
-    - list: List of queries with attributes based on the specified table expression type.
+        list: List of queries with attributes based on the specified table expression type.
     """
-
     case = table_expression_type
     table_expression_query = ""
     queries_with_attributes = []
 
-    # Generate table expression based on different cases
-    if isinstance(case, dict):  # case=={"single_table": "city"}
-        type_of_table = list(case.keys())[0]
-        table = case[type_of_table]
-
-        if type_of_table == "single_table":
-            # Single table expression
-            table_expression_query = f" FROM {table}"
-            attributes = all_colms(schema, schema_types, [table])
-            queries_with_attributes.append(
-                [table_expression_query, [table], attributes]
-            )
-
-        elif type_of_table == "single_table_with_name_changing":
-            # Single table expression with alias
-            alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
-            table_expression_query = f" FROM {table} AS {alias_name}"
-            attributes = all_colms(schema, schema_types, [table], [alias_name])
-
-            queries_with_attributes.append(
-                [table_expression_query, {alias_name: table}, attributes]
-            )
-
-    if case == "single_table":
-        # Iterate through all tables as single table expressions
-        if random_choice:
-            table = random.choice(list(schema.keys()))
-            table_expression_query = f" FROM {table}"
-            attributes = all_colms(schema, schema_types, [table])
-            queries_with_attributes.append(
-                [table_expression_query, [table], attributes]
-            )
-        else:
-            for table in schema:
-                table_expression_query = f" FROM {table}"
-                attributes = all_colms(schema, schema_types, [table])
-                queries_with_attributes.append(
-                    [table_expression_query, [table], attributes]
-                )
-
-    elif case == "single_table_with_name_changing":
-        # Iterate through all tables with an alias as single table expressions
-        if random_choice:
-            table = random.choice(list(schema.keys()))
-            alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
-            table_expression_query = f" FROM {table} AS {alias_name}"
-            attributes = all_colms(schema, schema_types, [table], [alias_name])
-
-            queries_with_attributes.append(
-                [table_expression_query, {alias_name: table}, attributes]
-            )
-        else:
-            for table in schema:
-                alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
-                table_expression_query = f" FROM {table} AS {alias_name}"
-                attributes = all_colms(schema, schema_types, [table], [alias_name])
-
-                queries_with_attributes.append(
-                    [table_expression_query, {alias_name: table}, attributes]
-                )
-
-    elif "JOIN" in case:
-        # Generate join queries based on the specified join types
-        num_joins = len(case.split("_"))
-        join_types = case.split("_")
-
-        if meaningful_joins == "yes":
-            # Generate meaningful join queries
-            temp_queries = generate_join_query(
-                schema, fk, join_types, random_choice=random_choice
-            )
-            for temp in temp_queries:
-                all_columns = all_colms(schema, schema_types, temp[1])
-                temp.append(all_columns)
-                queries_with_attributes.append(temp)
-
-        elif meaningful_joins == "no":
-            # Generate meaningless join queries
-            temp_queries = generate_meaningless_join(
-                schema, int(num_joins), join_types, random_choice=random_choice
-            )
-            for temp in temp_queries:
-                all_columns = all_colms(schema, schema_types, temp[1])
-                queries_with_attributes.append([temp[0], temp[1], all_columns])
-
-        else:
-            if random_choice:
-                meaningful_joins = "no"
-                temp_queries = generate_meaningless_join(
-                    schema, int(num_joins), join_types, random_choice=random_choice
-                )
-                for temp in temp_queries:
-                    all_columns = all_colms(schema, schema_types, temp[1])
-                    queries_with_attributes.append([temp[0], temp[1], all_columns])
-            else:
-                # Generate both meaningful and meaningless join queries
-                temp_queries = generate_meaningless_join(
-                    schema, int(num_joins), join_types, random_choice=random_choice
-                )
-                for temp in temp_queries:
-                    all_columns = all_colms(schema, schema_types, temp[1])
-                    queries_with_attributes.append([temp[0], temp[1], all_columns])
-
-                temp_queries = generate_join_query(
-                    schema, fk, join_types, random_choice=random_choice
-                )
-                for temp in temp_queries:
-                    all_columns = all_colms(schema, schema_types, temp[1])
-                    temp.append(all_columns)
-                    queries_with_attributes.append(temp)
-
+    if isinstance(
+        case, dict
+    ):  # case=={"single_table": "city"} mainly used for subquery
+        queries_with_attributes = handle_table_expression_for_subquery(
+            case, schema, schema_types, random_choice
+        )
+    elif case == "single_table":  # case=="single_table"
+        queries_with_attributes = handle_single_table_case(
+            schema, schema_types, random_choice
+        )
+    elif (
+        case == "single_table_with_name_changing"
+    ):  # case=="single_table_with_name_changing"
+        queries_with_attributes = handle_single_table_with_name_changing_case(
+            schema, schema_types, random_choice
+        )
+    elif "JOIN" in case:  # case=="INNER JOIN" or case=="INNER JOIN_RIGHT JOIN"
+        queries_with_attributes = handle_join_case(
+            case, schema, fk, schema_types, meaningful_joins, random_choice
+        )
     elif case == "CTE":
         pass
-
     elif case == "sub_query":
         pass
 
@@ -159,54 +76,54 @@ def create_table_expression(
 
 
 # Schema and keys
-schema = {
-    "city": [
-        "city_id",
-        "official_name",
-        "status",
-        "area_km_2",
-        "population",
-        "census_ranking",
-    ],
-    "farm": [
-        "farm_id",
-        "year",
-        "total_horses",
-        "working_horses",
-        "total_cattle",
-        "oxen",
-        "bulls",
-        "cows",
-        "pigs",
-        "sheep_and_goats",
-    ],
-    # Other tables and columns...
-}
+# schema = {
+#     "city": [
+#         "city_id",
+#         "official_name",
+#         "status",
+#         "area_km_2",
+#         "population",
+#         "census_ranking",
+#     ],
+#     "farm": [
+#         "farm_id",
+#         "year",
+#         "total_horses",
+#         "working_horses",
+#         "total_cattle",
+#         "oxen",
+#         "bulls",
+#         "cows",
+#         "pigs",
+#         "sheep_and_goats",
+#     ],
+#     # Other tables and columns...
+# }
 
-primary_keys = {
-    "city": "city_id",
-    "farm": "farm_id",
-    # Other primary keys...
-}
+# primary_keys = {
+#     "city": "city_id",
+#     "farm": "farm_id",
+#     # Other primary keys...
+# }
 
-foreign_keys = {
-    "farm_competition": {"host_city_id": ("city", "city_id")},
-    # Other foreign keys...
-}
+# foreign_keys = {
+#     "farm_competition": {"host_city_id": ("city", "city_id")},
+#     # Other foreign keys...
+# }
 
-schema_types = {
-    "city": {
-        "city_id": "number",
-        "official_name": "text",
-        # Other column types...
-    },
-    "farm": {
-        "farm_id": "number",
-        "year": "number",
-        # Other column types...
-    },
-    # Other tables and column types...
-}
+# schema_types = {
+#     "city": {
+#         "city_id": "number",
+#         "official_name": "text",
+#         # Other column types...
+#     },
+#     "farm": {
+#         "farm_id": "number",
+#         "year": "number",
+#         # Other column types...
+#     },
+#     # Other tables and column types...
+# }
 
 # Example function calls:
 # create_table_expression(schema, primary_keys, foreign_keys, "single_table")
---
+++ query_generation/table_expression/table_expression_helper_funcs.py
@@ -0,0 +1,384 @@
+import itertools
+import random
+import sys
+
+import networkx as nx
+
+sys.path.append("..")
+
+from helper_funcs import all_colms
+from join import generate_join_query, generate_meaningless_join
+
+
+def handle_table_expression_for_subquery(case, schema, schema_types, random_choice):
+    """
+    Handle the table expression for subquery case.
+
+    Args:
+        case (dict): The case specifying the table expression.
+        schema (dict): Database schema with table names and their columns.
+        schema_types (dict): Data types of columns in the schema.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        >>> schema = {
+        ...     "city": ["city_id", "name"],
+        ...     "country": ["country_id", "name"],
+        ... }
+        >>> schema_types = {
+        ...     "city": {"city_id": "int", "name": "str"},
+        ...     "country": {"country_id": "int", "name": "str"},
+        ... }
+        >>> case = {"single_table": "city"}
+        >>> handle_table_expression_for_subquery(case, schema, schema_types, random_choice=False)
+        [[' FROM city', ['city'], ['city_id', 'name']]]
+
+        >>> case = {"single_table_with_name_changing": "country"}
+        >>> handle_table_expression_for_subquery(case, schema, schema_types, random_choice=False)
+        [[' FROM country AS a', {'a': 'country'}, ['a.country_id', 'a.name']]]
+    """
+    type_of_table = list(case.keys())[0]
+    table = case[type_of_table]
+    queries_with_attributes = []
+
+    if type_of_table == "single_table":
+        table_expression_query = f" FROM {table}"
+        attributes = all_colms(schema, schema_types, [table])
+        queries_with_attributes.append([table_expression_query, [table], attributes])
+    elif type_of_table == "single_table_with_name_changing":
+        alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
+        table_expression_query = f" FROM {table} AS {alias_name}"
+        attributes = all_colms(schema, schema_types, [table], [alias_name])
+        queries_with_attributes.append(
+            [table_expression_query, {alias_name: table}, attributes]
+        )
+
+    return queries_with_attributes
+
+
+def handle_single_table_case(schema, schema_types, random_choice):
+    """
+    Handle the single table case.
+
+    Args:
+        schema (dict): Database schema with table names and their columns.
+        schema_types (dict): Data types of columns in the schema.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        >>> schema = {
+        ...     "city": ["city_id", "name"],
+        ...     "country": ["country_id", "name"],
+        ... }
+        >>> schema_types = {
+        ...     "city": {"city_id": "int", "name": "str"},
+        ...     "country": {"country_id": "int", "name": "str"},
+        ... }
+        >>> handle_single_table_case(schema, schema_types, random_choice=False)
+        [[' FROM city', ['city'], ['city_id', 'name']], [' FROM country', ['country'], ['country_id', 'name']]]
+    """
+    queries_with_attributes = []
+
+    if random_choice:
+        table = random.choice(list(schema.keys()))
+        table_expression_query = f" FROM {table}"
+        attributes = all_colms(schema, schema_types, [table])
+        queries_with_attributes.append([table_expression_query, [table], attributes])
+    else:
+        for table in schema:
+            table_expression_query = f" FROM {table}"
+            attributes = all_colms(schema, schema_types, [table])
+            queries_with_attributes.append(
+                [table_expression_query, [table], attributes]
+            )
+
+    return queries_with_attributes
+
+
+def handle_single_table_with_name_changing_case(schema, schema_types, random_choice):
+    """
+    Handle the single table with name changing case.
+
+    Args:
+        schema (dict): Database schema with table names and their columns.
+        schema_types (dict): Data types of columns in the schema.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        >>> schema = {
+        ...     "city": ["city_id", "name"],
+        ...     "country": ["country_id", "name"],
+        ... }
+        >>> schema_types = {
+        ...     "city": {"city_id": "int", "name": "str"},
+        ...     "country": {"country_id": "int", "name": "str"},
+        ... }
+        >>> handle_single_table_with_name_changing_case(schema, schema_types, random_choice=False)
+        [[' FROM city AS a', {'a': 'city'}, ['a.city_id', 'a.name']], [' FROM country AS b', {'b': 'country'}, ['b.country_id', 'b.name']]]
+
+    """
+    queries_with_attributes = []
+
+    if random_choice:
+        table = random.choice(list(schema.keys()))
+        alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
+        table_expression_query = f" FROM {table} AS {alias_name}"
+        attributes = all_colms(schema, schema_types, [table], [alias_name])
+        queries_with_attributes.append(
+            [table_expression_query, {alias_name: table}, attributes]
+        )
+    else:
+        for table in schema:
+            alias_name = random.choice("abcdefghijklmnopqrstuvwxyz")
+            table_expression_query = f" FROM {table} AS {alias_name}"
+            attributes = all_colms(schema, schema_types, [table], [alias_name])
+            queries_with_attributes.append(
+                [table_expression_query, {alias_name: table}, attributes]
+            )
+
+    return queries_with_attributes
+
+
+def handle_join_case(case, schema, fk, schema_types, meaningful_joins, random_choice):
+    """
+    Handle the join case.
+
+    Args:
+        case (str): The case specifying the table expression.
+        schema (dict): Database schema with table names and their columns.
+        fk (dict): Foreign keys for tables.
+        schema_types (dict): Data types of columns in the schema.
+        meaningful_joins (str): Type of join - meaningful or meaningless.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        Examples:
+        >>> schema = {
+        ...     "city": [
+        ...         "city_id",
+        ...         "official_name",
+        ...         "status",
+        ...         "area_km_2",
+        ...         "population",
+        ...         "census_ranking",
+        ...     ],
+        ...     "farm": [
+        ...         "farm_id",
+        ...         "year",
+        ...         "total_horses",
+        ...         "working_horses",
+        ...         "total_cattle",
+        ...         "oxen",
+        ...         "bulls",
+        ...         "cows",
+        ...         "pigs",
+        ...         "sheep_and_goats",
+        ...     ],
+        ...     "farm_competition": ["Competition_ID", "Year", "Theme", "Host_city_ID", "Hosts"],
+                "competition_record": ["Competition_ID", "Farm_ID", "Rank"],
+        ... }
+        >>> fk = {
+        ...     "farm_competition": {"host_city_id": ("city", "city_id")},
+        ...     # Other foreign keys...
+        ... }
+        >>> schema_types = {
+        ...     "city": {
+        ...         "city_id": "number",
+        ...         "official_name": "text",
+        ...         # Other column types...
+        ...     },
+        ...     "farm": {
+        ...         "farm_id": "number",
+        ...         "year": "number",
+        ...         # Other column types...
+        ...     },
+        ...     # Other tables and column types...
+        ... }
+        >>> handle_join_case("INNER_JOIN", schema, fk, schema_types, "yes", random_choice=False)
+        [[FROM city INNER JOIN farm_competition ON city.city_id = farm_competition.host_city_id', ['city', 'farm_competition']]
+
+    """
+    queries_with_attributes = []
+
+    num_joins = len(case.split("_"))
+    join_types = case.split("_")
+
+    if meaningful_joins == "yes":
+        queries_with_attributes = handle_meaningful_joins(
+            schema, fk, join_types, schema_types, random_choice
+        )
+    elif meaningful_joins == "no":
+        queries_with_attributes = handle_meaningless_joins(
+            schema, num_joins, join_types, schema_types, random_choice
+        )
+    elif random_choice:
+        meaningful_joins = "no"
+        queries_with_attributes = handle_meaningless_joins(
+            schema, num_joins, join_types, schema_types, random_choice
+        )
+    else:
+        queries_with_attributes = handle_meaningless_joins(
+            schema, num_joins, join_types, schema_types, random_choice
+        )
+
+        temp_queries = generate_join_query(
+            schema, fk, join_types, random_choice=random_choice
+        )
+        for temp in temp_queries:
+            all_columns = all_colms(schema, schema_types, temp[1])
+            temp.append(all_columns)
+            queries_with_attributes.append(temp)
+    print(queries_with_attributes)
+    return queries_with_attributes
+
+
+def handle_meaningful_joins(schema, fk, join_types, schema_types, random_choice):
+    """
+    Handle the meaningful joins.
+
+    Args:
+        schema (dict): Database schema with table names and their columns.
+        fk (dict): Foreign keys for tables.
+        join_types (list): List of join types.
+        schema_types (dict): Data types of columns in the schema.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        >>> schema = {
+        ...     "city": [
+        ...         "city_id",
+        ...         "official_name",
+        ...         "status",
+        ...         "area_km_2",
+        ...         "population",
+        ...         "census_ranking",
+        ...     ],
+        ...     "farm": [
+        ...         "farm_id",
+        ...         "year",
+        ...         "total_horses",
+        ...         "working_horses",
+        ...         "total_cattle",
+        ...         "oxen",
+        ...         "bulls",
+        ...         "cows",
+        ...         "pigs",
+        ...         "sheep_and_goats",
+        ...     ],
+        ...     # Other tables and columns...
+        ... }
+        >>> fk = {
+        ...     "farm_competition": {"host_city_id": ("city", "city_id")},
+        ...     # Other foreign keys...
+        ... }
+        >>> schema_types = {
+        ...     "city": {
+        ...         "city_id": "number",
+        ...         "official_name": "text",
+        ...         # Other column types...
+        ...     },
+        ...     "farm": {
+        ...         "farm_id": "number",
+        ...         "year": "number",
+        ...         # Other column types...
+        ...     },
+        ...     # Other tables and column types...
+        ... }
+        >>> join_types = ["INNER JOIN", "LEFT JOIN"]
+        >>> handle_meaningful_joins(schema, fk, join_types, schema_types, random_choice=False)
+        [[' FROM city INNER JOIN farm_competition ON city.city_id = farm_competition.host_city_id', ['city', 'farm_competition']]]
+
+    """
+    temp_queries = generate_join_query(
+        schema, fk, join_types, random_choice=random_choice
+    )
+    queries_with_attributes = []
+    for temp in temp_queries:
+        all_columns = all_colms(schema, schema_types, temp[1])
+        temp.append(all_columns)
+        queries_with_attributes.append(temp)
+    return queries_with_attributes
+
+
+def handle_meaningless_joins(
+    schema, num_joins, join_types, schema_types, random_choice
+):
+    """
+    Handle the meaningless joins.
+
+    Args:
+        schema (dict): Database schema with table names and their columns.
+        num_joins (int): Number of joins to generate.
+        join_types (list): List of join types.
+        schema_types (dict): Data types of columns in the schema.
+        random_choice (bool): Indicates if random choice is enabled.
+
+    Returns:
+        list: List of queries with attributes based on the specified table expression type.
+
+    Examples:
+        >>> schema = {
+        ...     "city": [
+        ...         "city_id",
+        ...         "official_name",
+        ...         "status",
+        ...         "area_km_2",
+        ...         "population",
+        ...         "census_ranking",
+        ...     ],
+        ...     "farm": [
+        ...         "farm_id",
+        ...         "year",
+        ...         "total_horses",
+        ...         "working_horses",
+        ...         "total_cattle",
+        ...         "oxen",
+        ...         "bulls",
+        ...         "cows",
+        ...         "pigs",
+        ...         "sheep_and_goats",
+        ...     ],
+        ...     # Other tables and columns...
+        ... }
+        >>> schema_types = {
+        ...     "city": {
+        ...         "city_id": "number",
+        ...         "official_name": "text",
+        ...         # Other column types...
+        ...     },
+        ...     "farm": {
+        ...         "farm_id": "number",
+        ...         "year": "number",
+        ...         # Other column types...
+        ...     },
+        ...     # Other tables and column types...
+        ... }
+        >>> num_joins = 2
+        >>> join_types = ["INNER JOIN", "LEFT JOIN"]
+        >>> handle_meaningless_joins(schema, num_joins, join_types, schema_types, random_choice=False)
+        [[' FROM city INNER JOIN farm ON city.city_id = farm.farm_id', ['city', 'farm']]]
+
+    """
+    temp_queries = generate_meaningless_join(
+        schema, num_joins, join_types, random_choice=random_choice
+    )
+    queries_with_attributes = []
+    for temp in temp_queries:
+        all_columns = all_colms(schema, schema_types, temp[1])
+        queries_with_attributes.append([temp[0], temp[1], all_columns])
+    return queries_with_attributes
